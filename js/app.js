df2eb0b776c8673b750247bf88acb9d838a3df05
Ram Parthasarathy <ramanujamp@gmail.com>
2011-11-05 23:56:36 -0400

    including backbone.js, underscore.js and creating initial input field

---
+++ js/app.js
@@ -0,0 +1,34 @@
+App = {
+  start: function() {
+    new App.TasksView()
+  }
+}
+
+
+App.TasksView = Backbone.View.extend ({
+  el: '#tasks',
+
+  events: {
+    'keypress': 'handleEnter'
+  },
+
+  initialize: function(){
+    $(this.el).focus()
+  },
+
+  handleEnter: function(e){
+    if (e.keyCode == 13) {
+      console.log('Enter');
+
+      // call a function
+      // json stringify
+      // id it
+      // look at localstorage router
+      var myPost = $(this.el).val();
+      var contents = $(".tasklist").append("<li>" + myPost + "</li>").html();
+      localStorage.setItem('myTasks', contents);
+    }
+  }
+
+
+})
