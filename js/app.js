4b3ef662c0792400d54b20a0a816c9d4e8775471
Ramanujam <mail.ramanujam@gmail.com>
2012-02-29 13:31:03 -0500

    Add comments

--- js/app.js
+++ js/app.js
@@ -5,6 +5,27 @@ App = {
 }
 
 
+  /*
+   localStorage.setItem('foo', 'bar');
+   localStorage.getItem('foo');
+
+   Keyup 
+   http://api.jquery.com/keyup/
+   keycode for Enter is 13
+  
+   Click
+   http://api.jquery.com/click/
+
+   Get entered value and append to the li
+   Set the value to a localstorage key value pair
+   Get the localstorage value and append it to li
+
+   Optional
+   Strikethrough and remove on click?
+    
+
+*/ 
+
 App.TasksView = Backbone.View.extend ({
   el: '#tasks',
 
@@ -20,9 +41,6 @@ App.TasksView = Backbone.View.extend ({
     if (e.keyCode == 13) {
       console.log('Enter');
 
-      // call a function
-      // json stringify
-      // look at localstorage router
       var myPost = $(this.el).val();
       var contents = $(".tasklist").append("<li>" + myPost + "</li>").html();
       localStorage.setItem('myTasks', contents);
